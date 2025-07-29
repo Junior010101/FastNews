7f7cffa2f41be455bd0ad2024410804633bf7bdf
junior010101 <marcondesjunior2209@gmail.com>
2025-07-29 03:13:55 -0300

    Enviando Cabeçãlho

--- site/src/app/app.ts
+++ site/src/app/app.ts
@@ -1,14 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Home } from './pages/home/home';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet, Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
-  protected readonly title = signal('site');
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly titleService = inject(Title);
+
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => {
+        let route = this.activatedRoute;
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      mergeMap(route => route.data)
+    ).subscribe(data => {
+      if (data['title']) {
+        this.titleService.setTitle(data['title']);
+      }
+    });
+  }
 }
---
+++ site/src/app/pages/noticia/noticia.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noticia',
+  imports: [],
+  templateUrl: './noticia.html',
+  styleUrl: './noticia.scss'
+})
+export class Noticia {
+
+}
