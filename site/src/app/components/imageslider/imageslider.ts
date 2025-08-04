388ed56a7112e291fc87a42f44b95833a8fc413e
junior010101 <marcondesjunior2209@gmail.com>
2025-08-04 01:21:25 -0300

    Atualizando componente ImagePreview

--- site/src/app/components/imageslider/imageslider.ts
+++ site/src/app/components/imageslider/imageslider.ts
@@ -6,20 +6,23 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './imageslider.html',
-  styleUrls: ['./imageslider.scss']
+  styleUrls: ['./imageslider.scss'],
 })
 export class Imageslider {
   slides = [
     {
       title: 'Algo',
       data: '00/00/0000',
-      image: 'https://www.pontotel.com.br/local/wp-content/uploads/2022/05/imagem-corporativa.webp'
+      image:
+        'https://www.pontotel.com.br/local/wp-content/uploads/2022/05/imagem-corporativa.webp',
     },
     {
       title: 'Outra coisa',
       data: '00/00/0000',
-      image: 'https://wallpapers.com/images/hd/nature-pictures-ffm1xnpcdzxox0y7.jpg'
+      image:
+        'https://wallpapers.com/images/hd/nature-pictures-ffm1xnpcdzxox0y7.jpg',
     },
+
     // adiciona outros slides aqui
   ];
 
